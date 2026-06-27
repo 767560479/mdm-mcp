99ee6098df1f5c8d1e1b73c78becc5e0a3d98086
谭正飞 <zhengfei.tan@definesys.com>
2026-06-27 20:46:28 +0800

    Add .env.example file, update .gitignore, and include dotenv dependency

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import "dotenv/config";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { isHttpTransport, loadConfig } from "./config.js";
 import { startHttpServer } from "./http-server.js";
